4f0a942f840b808ac12cc08ed91b8d76e348587f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-04-23 08:40:24 -0700

    movie class init

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,5 @@
 import media
 
-toy_story = media.Movie()
+toy_story = media.Movie('Toy Story', 'A story about toys', 'https://images-na.ssl-images-amazon.com/images/I/514IG81HAhL.jpg', 'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+
+print(toy_story.storyline)
