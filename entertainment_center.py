cd14c42e95c9e173ff7585a0af483255698f6965
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-04-24 08:34:47 -0700

    working on the movies page

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,11 +1,17 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie('Toy Story', 'A story about toys', 'https://images-na.ssl-images-amazon.com/images/I/514IG81HAhL.jpg', 'https://www.youtube.com/watch?v=KYz2wyBy3kc')
-
-print(toy_story.storyline)
-
 avatar = media.Movie('Avatar', 'A story about avatar', 'link', 'link')
+school_of_rock = media.Movie('School of Rock', 'A story about music', 'link', 'link')
+ratatouille = media.Movie('Ratatouille', 'A story about a rat chef', 'link', 'link')
+hunger_games = media.Movie('Hunger Games', 'A story about reality show', 'link', 'link')
 
-print(avatar.storyline)
+movies = [toy_story, avatar, school_of_rock, ratatouille, hunger_games]
+fresh_tomatoes.open_movies_page(movies)
+# print(toy_story.storyline)
 
-toy_story.show_trailer()
+
+# print(avatar.storyline)
+
+# toy_story.show_trailer()
